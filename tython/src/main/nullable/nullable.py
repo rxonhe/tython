7ae5db886f5f1ae7964c68987279ba94732d4b4f
Choinhet <choinhet@gmail.com>
2024-01-17 19:26:21 -0300

    feat: impl nullable and tests

--- tython/src/main/nullable/nullable.py
+++ tython/src/main/nullable/nullable.py
@@ -1,6 +1,24 @@
-class Nullable(None):
-    def __init__(self, contained):
-        self.contained = contained
+from __future__ import annotations
 
-    def __getitem__(self, item):
-        return Nullable(item)
+import inspect
+from typing import Any
+
+
+class Nullable:
+    _init_type = Any
+
+    def __init__(self, contained=None):
+        if isinstance(contained, type):
+            self._init_type = contained
+            contained = None
+        self._contained = contained
+
+    def __getattr__(self, name):
+        _contained_vars = dict(vars(self._init_type))
+        if name not in _contained_vars:
+            raise AttributeError(f"Attribute/Method {name} not found for type {self._init_type}")
+        name_ = _contained_vars[name]
+        _is_method = inspect.ismethoddescriptor(name_) or inspect.isfunction(name_) or inspect.ismethod(name_)
+        if _is_method:
+            return getattr(self._contained, name, lambda *args, **kwargs: None)
+        return getattr(self._contained, name, None)
--- tython/src/test/nullable/test_nullable.py
+++ tython/src/test/nullable/test_nullable.py
@@ -1,5 +1,38 @@
 import unittest
+from dataclasses import dataclass
+from typing import Union
+
+from tython.src.main.nullable.nullable import Nullable
+
+
+class ChildClass:
+    my_property = "property"
+
+    @staticmethod
+    def my_method():
+        return "method"
+
+
+@dataclass
+class ParentClass:
+    my_int: int
+    my_child: Union[ChildClass, Nullable] = Nullable(ChildClass)
 
 
 class TestNullable(unittest.TestCase):
-    ...
+
+    def test_property_access_on_nullable(self):
+        parent = ParentClass(1)
+        self.assertEqual(parent.my_child.my_property, None)
+
+    def test_method_access_on_nullable(self):
+        parent = ParentClass(1)
+        self.assertEqual(parent.my_child.my_method(), None)
+
+    def test_property_access_on_valid(self):
+        parent = ParentClass(1, ChildClass())
+        self.assertEqual(parent.my_child.my_property, "property")
+
+    def test_method_access_on_valid(self):
+        parent = ParentClass(1, ChildClass())
+        self.assertEqual(parent.my_child.my_method(), "method")
